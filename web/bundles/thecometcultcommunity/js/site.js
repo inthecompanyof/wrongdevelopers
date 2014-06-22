d7fc6ca94ec50dae1362818ba2e2b19d7a3e700c
Vagrant User <vagrant@community-aggregator.dev>
2014-06-22 10:00:30 +0000

    fb rename

--- web/bundles/thecometcultcommunity/js/site.js
+++ web/bundles/thecometcultcommunity/js/site.js
@@ -2,7 +2,7 @@ var FB_user;
 
  window.fbAsyncInit = function() {
     FB.init({
-        appId      : '6517963728315334',
+        appId      : '517963728315334',
         status     : true,
         xfbml      : false
     });
